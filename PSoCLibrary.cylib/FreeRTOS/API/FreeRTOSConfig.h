f18132df9b3d85425bd2a92aa9f43648ba1df5ec
Alan Hawse <arh@Alans-MacBook-Pro.local>
2015-12-20 16:02:05 -0500

    Updated more paramters

--- PSoCLibrary.cylib/FreeRTOS/API/FreeRTOSConfig.h
+++ PSoCLibrary.cylib/FreeRTOS/API/FreeRTOSConfig.h
@@ -89,42 +89,36 @@
 #define configCPU_CLOCK_HZ          ( ( unsigned long ) CYDEV_BCLK__SYSCLK__HZ )
         
 /* Application settings - user editable */
-#define configMAX_PRIORITIES		( 3 )
-#define configTOTAL_HEAP_SIZE		( (size_t) (2048) )
-    
-//    #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 100 )
-//#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 2048 ) )
-    
-//#define configUSE_PREEMPTION			1
+#define configMAX_PRIORITIES		( `$CMaxPriorities` )
+#define configTOTAL_HEAP_SIZE		( (size_t) (`$HeapSize`) )
 
 #define configUSE_PREEMPTION			`$CUsePreEmption`
-    
-#define configUSE_IDLE_HOOK				0
-#define configUSE_TICK_HOOK				0
-#define configTICK_RATE_HZ				( ( portTickType ) 1000 )
-#define configMINIMAL_STACK_SIZE		( 40 )
-#define configMAX_TASK_NAME_LEN			( 12 )
-#define configUSE_TRACE_FACILITY		0
-#define configUSE_16_BIT_TICKS			0
-#define configIDLE_SHOULD_YIELD			0
-#define configUSE_MUTEXES				1
+#define configUSE_IDLE_HOOK				`$CUseIdleHook`
+#define configUSE_TICK_HOOK				`$CUseTickHook`
+#define configTICK_RATE_HZ				( ( portTickType ) `$CTickRateHz` )
+#define configMINIMAL_STACK_SIZE		( `$CMinimalStackSize` )
+#define configMAX_TASK_NAME_LEN			( `$CMaxTaskNameLen` )
+#define configUSE_TRACE_FACILITY		`$CUseTraceFacility`
+#define configUSE_16_BIT_TICKS			`$CUse16BitTicks`
+#define configIDLE_SHOULD_YIELD			`$CIdleShouldYield`
+#define configUSE_MUTEXES				`$CUsesMutexes`
 #define configQUEUE_REGISTRY_SIZE		0
-#define configCHECK_FOR_STACK_OVERFLOW	2
-#define configUSE_RECURSIVE_MUTEXES		0
-#define configUSE_MALLOC_FAILED_HOOK	1
-#define configUSE_APPLICATION_TASK_TAG	0
-#define configUSE_COUNTING_SEMAPHORES	1
-#define configGENERATE_RUN_TIME_STATS	0
-#define configUSE_TIME_SLICING          1
+#define configCHECK_FOR_STACK_OVERFLOW	`$CCheckForStackOverflow`
+#define configUSE_RECURSIVE_MUTEXES		`$CUseRecursiveMutexes`
+#define configUSE_MALLOC_FAILED_HOOK	`$CMallocFailedHook`
+#define configUSE_APPLICATION_TASK_TAG	`$CUseApplicationTaskTag`
+#define configUSE_COUNTING_SEMAPHORES	`$CUseCountingSemaphores`
+#define configGENERATE_RUN_TIME_STATS	`$CGenerateRunTimeStats`
+#define configUSE_TIME_SLICING          `$CUseTimeSlicing`
 
 /* Co-routine definitions. */
-#define configUSE_CO_ROUTINES 			0
-#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
+#define configUSE_CO_ROUTINES 			`$CUseCoRoutines`
+#define configMAX_CO_ROUTINE_PRIORITIES ( `$CMaxCoRoutinePriorities` )
 
 /* Software timer definitions. */
-#define configUSE_TIMERS				1
-#define configTIMER_TASK_PRIORITY		2
-#define configTIMER_QUEUE_LENGTH		2
+#define configUSE_TIMERS				`$CUseTimers`
+#define configTIMER_TASK_PRIORITY		`$CTimerTaskPriority`
+#define configTIMER_QUEUE_LENGTH		`$CTimerQueueLength`
 #define configTIMER_TASK_STACK_DEPTH	( 2 * configMINIMAL_STACK_SIZE )
 
 /*
@@ -160,17 +154,17 @@ Notes:
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
-#define INCLUDE_vTaskPrioritySet		        0
-#define INCLUDE_uxTaskPriorityGet		        0
-#define INCLUDE_vTaskDelete				        0
-#define INCLUDE_vTaskCleanUpResources	        0
-#define INCLUDE_vTaskSuspend			        0
-#define INCLUDE_vTaskDelayUntil			        0
-#define INCLUDE_vTaskDelay				        1
-#define INCLUDE_uxTaskGetStackHighWaterMark	    0
-#define INCLUDE_xTimerPendFunctionCallFromISR   0
-#define INCLUDE_xEventGroupSetBitFromISR        0
-#define INCLUDE_xTimerPendFunctionCall          0
+#define INCLUDE_vTaskPrioritySet		        `$IVTaskPrioritySet`
+#define INCLUDE_uxTaskPriorityGet		        `$IUXTaskPriorityGet`
+#define INCLUDE_vTaskDelete				        `$IVTaskDelete`
+#define INCLUDE_vTaskCleanUpResources	        `$IVtaskCleanUpResources`
+#define INCLUDE_vTaskSuspend			        `$IVTaskSuspend`
+#define INCLUDE_vTaskDelayUntil			        `$IVTaskDelayUntil`
+#define INCLUDE_vTaskDelay				        `$IVTaskDelay`
+#define INCLUDE_uxTaskGetStackHighWaterMark	    `$IUXTaskGetStackHighWaterMark`
+#define INCLUDE_xTimerPendFunctionCallFromISR   `$IXTimerPendFunctionCallFromISR`
+#define INCLUDE_xEventGroupSetBitFromISR        `$IXEventGroupSetBitFromISR`
+#define INCLUDE_xTimerPendFunctionCall          `$IXTimerPendFunctionCall`
 
 /* Normal assert() semantics without relying on the provision of an assert.h
 header file. */
